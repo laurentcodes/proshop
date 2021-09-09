078dad8f36d9b1d8b47f61ecc0dd06443a1313c4
laurentcodes <stlaurentcodes@gmail.com>
2021-09-09 12:25:23 +0100

    Updates

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -80,25 +80,10 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
 // @desc    Get logged in user orders
 // @route   GET /api/orders/myorders
 // @access  Private
-const updateOrderToPaid = asyncHandler(async (req, res) => {
+const getMyOrders = asyncHandler(async (req, res) => {
 	const orders = await Order.find({ user: req.user._id });
 
-	if (order) {
-		order.isPaid = true;
-		order.paidAt = Date.now();
-		order.paymentResult = {
-			id: req.body.id,
-			status: req.body.status,
-			update_time: req.body.update_time,
-			email_address: req.body.payer.email_address,
-		};
-
-		const updatedOrder = await order.save();
-		res.json(updatedOrder);
-	} else {
-		res.status(404);
-		throw Error('Order Not Found');
-	}
+	res.json(orders);
 });
 
-export { addOrderItems, getOrderById, updateOrderToPaid };
+export { addOrderItems, getOrderById, updateOrderToPaid, getMyOrders };
